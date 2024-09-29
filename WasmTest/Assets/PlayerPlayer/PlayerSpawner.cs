f47368305798768339ad90747f890cb39c169b2c
Amelia <60483028+Miabread@users.noreply.github.com>
2024-09-29 16:04:52 -0400

    Visual changes

--- WasmTest/Assets/PlayerPlayer/PlayerSpawner.cs
+++ WasmTest/Assets/PlayerPlayer/PlayerSpawner.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 
 public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
 {
+    public GameObject canvas;
     public GameObject PlayerPrefab;
+
+    public void Awake()
+    {
+        canvas.SetActive(false);
+    }
 
     public void PlayerJoined(PlayerRef player)
     {
         if (player == Runner.LocalPlayer)
         {
+            canvas.SetActive(true);
             Runner.Spawn(PlayerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
         }
     }
